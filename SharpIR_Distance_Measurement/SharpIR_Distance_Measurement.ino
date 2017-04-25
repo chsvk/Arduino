ab6f6c2f62e22ed0ebceeff523e71c14d602d356
Vamshi Krishna <chsvk21@gmail.com>
2017-04-25 21:37:14 +0530

    Add files via upload
    Distance Measurement Algorithms for Ultrasonic Sensor and Sharp IR Sensor

---
+++ SharpIR_Distance_Measurement/SharpIR_Distance_Measurement.ino
@@ -0,0 +1,25 @@
+
+
+// Sharp IR GP2Y0A41SK0F Distance Test
+// Sharp IR GP2Y0A41SK0F (4-30cm, analog)
+#define sensor A0
+
+void setup() {
+  // start the serial port
+  Serial.begin(9600); 
+}
+
+void loop() {
+  
+  // value from sensor * (5/1024)
+  float volts = analogRead(sensor)*0.0048828125;  
+  // worked out from datasheet graph
+  int distance = 13*pow(volts, -1); 
+  // slow down serial port 
+  delay(1000); 
+  
+  if (distance <= 30){
+    // print the distance
+    Serial.println(distance);     
+    }
+}
---
+++ Ultrasonic_Sensor_Distance_Measurement/Ultrasonic_Sensor_Distance_Measurement.ino
@@ -0,0 +1,40 @@
+
+
+// Ping Sensor / Ultrasonic Sensor Test
+#define trigPin 10
+#define echoPin 11
+
+long distance,duration,distance2,duration2;
+void setup() {
+  // intializing the serial monitor
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  distance = check_distance();
+  // finding out the distance and printing it out to the serial monitor.
+    Serial.print(distance);
+    Serial.print("cm, ");
+    Serial.println();
+    delay(2);
+} 
+
+long check_distance()
+{
+  // Giving short pulses to the sensor to read the values and determine the distance of the object
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  // calculating the distance
+  distance = (duration/2) / 29.1;
+  delay(200);
+  return distance;
+}
+
+
